22a0beeb7f79119ade1e765cacac4a9155705b08
Shardul Bankar <shardulsb08@gmail.com>
2026-03-29 16:16:50 +0530

    fs/apfs: centralize stub errno and improve mount warning

---
+++ linux/fs/apfs/apfs.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef _FS_APFS_APFS_H
+#define _FS_APFS_APFS_H
+
+#include <linux/errno.h>
+
+#define APFS_STUB_MSG "APFS support is not available in this kernel build"
+#define APFS_STUB_ERRNO EOPNOTSUPP
+
+#endif /* _FS_APFS_APFS_H */
