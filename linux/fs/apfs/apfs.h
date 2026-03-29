bbc28ba3c7ce1f436459ed4148b3ecd8e554a1bc
Shardul Bankar <shardulsb08@gmail.com>
2026-03-29 16:38:55 +0530

    fs/apfs: centralize stub errno and enrich mount warning

--- linux/fs/apfs/apfs.h
+++ linux/fs/apfs/apfs.h
@@ -2,6 +2,9 @@
 #ifndef _FS_APFS_APFS_H
 #define _FS_APFS_APFS_H
 
+#include <linux/errno.h>
+
 #define APFS_STUB_MSG "APFS support is not available in this kernel build"
+#define APFS_STUB_ERRNO EOPNOTSUPP
 
 #endif /* _FS_APFS_APFS_H */
--- linux/fs/apfs/super.c
+++ linux/fs/apfs/super.c
@@ -8,14 +8,14 @@
 
 #include <linux/fs.h>
 #include <linux/module.h>
-
 #include "apfs.h"
 
 static struct dentry *apfs_mount(struct file_system_type *fs_type, int flags,
 				 const char *dev_name, void *data)
 {
-	pr_warn_once("apfs: %s\n", APFS_STUB_MSG);
-	return ERR_PTR(-EOPNOTSUPP);
+	pr_warn_once("apfs: %s (source=%s)\n", APFS_STUB_MSG,
+		     dev_name ? dev_name : "<none>");
+	return ERR_PTR(-APFS_STUB_ERRNO);
 }
 
 static void apfs_kill_sb(struct super_block *sb)
