1351259a8e1b6b40b6ca04c626c480e64908315b
Shardul Bankar <shardulsb08@gmail.com>
2026-03-29 17:44:41 +0530

    fs/apfs: split fs_context scaffold into options module

--- linux/fs/apfs/apfs.h
+++ linux/fs/apfs/apfs.h
@@ -3,8 +3,21 @@
 #define _FS_APFS_APFS_H
 
 #include <linux/errno.h>
+#include <linux/fs_context.h>
 
 #define APFS_STUB_MSG "APFS support is not available in this kernel build"
 #define APFS_STUB_ERRNO EOPNOTSUPP
+
+struct apfs_fs_context {
+	bool readwrite;
+	bool cknodes;
+	u32 vol;
+	u32 uid;
+	u32 gid;
+	char *snap;
+	char *tier2;
+};
+
+int apfs_init_fs_context(struct fs_context *fc);
 
 #endif /* _FS_APFS_APFS_H */
